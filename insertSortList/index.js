f5892b5dc3a26820c46c85cd0cd5f8463e443cc5
Li Yu <evanlee_@outlook.com>
2020-03-28 19:15:53 +0800

    re-understand the #insertSortList

--- insertSortList/index.js
+++ insertSortList/index.js
@@ -10,24 +10,29 @@
  * @return {ListNode}
  */
 var insertionSortList = function(head) {
-  const dummy = new ListNode(0)
- dummy.next = head
- let head0 = dummy.next
+  if (!head || !head.next) return head;
 
- while (head0 && head0.next) {
-   if (head0.next.val >= head0.val) {
-     head0 = head0.next
-     continue
-   }
+  //需要一个前继节点，这个节点用来记录排序好的链表的初始位置, 如果没有这个节点，无法在head处插入节点。
+  const node = new ListNode(0);
+  node.next = head;
+  let currentPosition = head, forward = node;
 
-   let pre = dummy
-   while (pre.next.val < head0.next.val) { pre = pre.next }
-
-   let next = head0.next
-   head0.next = next.next
-   next.next = pre.next
-   pre.next = next
- }
-
- return dummy.next;
-};
+  //找出next值小于当前值的节点
+  while(currentPosition && currentPosition.next) {
+    if (currentPosition.next.val >= currentPosition.val) {
+        currentPosition = currentPosition.next;
+        continue;
+    }
+    // 到这里,找到了next值小于当前值的节点，需在当前值之前找一个合适的地方放它的next，所以需要再遍历一次
+    forward = node;
+    while(forward.next.val < currentPosition.next.val) {
+        forward = forward.next;
+    }
+    //在forward和forward.next之间是currentPosition的next应该插入的位置
+    const temp = forward.next;
+    forward.next = currentPosition.next;
+    currentPosition.next = currentPosition.next.next;
+    forward.next.next = temp;
+  }
+  return node.next;
+}
